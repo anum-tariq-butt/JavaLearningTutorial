a3a35789753731866b73afe9068f651395dce3f9
Ahmad-younas <ahmadyounas2k18@gmail.com>
2024-12-03 00:46:02 +0500

    First Commit

--- src/main/java/org/example/Java8Feature/Student.java
+++ src/main/java/org/example/Java8Feature/Student.java
@@ -1,4 +1,26 @@
 package org.example.Java8Feature;
 
-public class Student {
+public class Student implements Comparable<Student>{
+    int id;
+    String name;
+
+    public Student(int id, String name){
+        this.name = name;
+        this.id = id;
+    }
+
+
+    @Override
+    public int compareTo(Student other) {
+        return this.id - other.id; // Sort by ID in ascending order
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
 }
