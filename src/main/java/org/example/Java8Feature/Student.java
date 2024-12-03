1af7347cb60af105c63320c7833f9c54918aabe2
anum-tariq-butt <anumbutt15@gmail.com>
2024-12-03 13:45:26 +0500

    SW-0910 Issue Resolved

--- src/main/java/org/example/Java8Feature/Student.java
+++ src/main/java/org/example/Java8Feature/Student.java
@@ -1,5 +1,7 @@
 package org.example.Java8Feature;
 
+import java.util.Objects;
+
 public class Student implements Comparable<Student>{
     int id;
     String name;
@@ -23,4 +25,16 @@ public class Student implements Comparable<Student>{
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
